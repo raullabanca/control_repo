56d0372c54323246cd49157bcf0acd7a90e9921b
raullabanca <raullabanca@yahoo.com.br>
2019-09-22 19:04:05 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -9,9 +9,9 @@ class profile::ssh_server {
   }
   
   ssh_authorized_key { 'root@master.puppet.vm':
-    ensure => present
-    user   => 'root'
-    type   => 'ssh-rsa'
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqzSvC1bzM+z6nSlHUklDD/+mVBH8xnvKZepkVDLMUXnGqFFP94rrLLiovUcpRzoVNey8GzJboOirKDhyEVacsNM9XYPfOqdDBY+N8RU3HwEcVARLe4MZbvqy020CptoO8on8Cw2KCHToNPW2MG5UoDg5S5Xz9MWdTk1ZXpFX+beHUBp5J8bPE8dIfMyt3VaKGK7Hq20FlOh5CGiA+zmVHO70Ulp74zEjii+ygwGN4uUOtGMt0ewUaZLmoxT5qUtKP+ujOHTEFiWpUmUKPIK6fwM2x45ZnR/QjUo0Y0/ljd534vqSEH6oLCx+AtZzDnMBEDwfrufG/HjD3Vqm6rLL9'
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqzSvC1bzM+z6nSlHUklDD/+mVBH8xnvKZepkVDLMUXnGqFFP94rrLLiovUcpRzoVNey8GzJboOirKDhyEVacsNM9XYPfOqdDBY+N8RU3HwEcVARLe4MZbvqy020CptoO8on8Cw2KCHToNPW2MG5UoDg5S5Xz9MWdTk1ZXpFX+beHUBp5J8bPE8dIfMyt3VaKGK7Hq20FlOh5CGiA+zmVHO70Ulp74zEjii+ygwGN4uUOtGMt0ewUaZLmoxT5qUtKP+ujOHTEFiWpUmUKPIK6fwM2x45ZnR/QjUo0Y0/ljd534vqSEH6oLCx+AtZzDnMBEDwfrufG/HjD3Vqm6rLL9',
   }
 }
