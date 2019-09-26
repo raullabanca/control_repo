e2db7ae85b48d038abf7369c40bc964fb9ecf8a1
raullabanca <raullabanca@yahoo.com.br>
2019-09-26 13:57:11 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,14 +5,14 @@ node 'master.puppet.vm' {
   include role::master_server
 }
 
+node 'minecraft.puppet.vm' {
+  include role::minecraft_server
+}
+
 node /^web/ {
   include role::app_server
 }
 
 node /^db/ {
   include role::db_server
-}
-
-node 'minecraft.puppet.vm' {
-  include role::minecraft_server
 }
