b67053780badc698fe8b394f9986ccc3d788391a
raullabanca <raullabanca@yahoo.com.br>
2019-09-26 13:43:21 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,6 +19,11 @@ class minecraft {
   
   file { '/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => puppet:///modules/minecraft/minecraft.service
+    source => puppet:///modules/minecraft/minecraft.service,
+  }
+  
+  service { 'minecraft':
+    ensure => running,
+    enable => true,
   }
 }
